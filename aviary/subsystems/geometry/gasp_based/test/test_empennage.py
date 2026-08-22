9502f4098cbba3d4d4b971760f4c89d7a2b013a2
Suhas <139655090+suhaslord@users.noreply.github.com>
2026-08-21 17:52:47 -0700

    test: make empennage options explicit

--- aviary/subsystems/geometry/gasp_based/test/test_empennage.py
+++ aviary/subsystems/geometry/gasp_based/test/test_empennage.py
@@ -5,8 +5,8 @@ from openmdao.utils.assert_utils import assert_check_partials, assert_near_equal
 from openmdao.utils.testing_utils import use_tempdirs
 
 from aviary.subsystems.geometry.gasp_based.empennage import EmpennageSize, TailSize, TailVolCoef
+from aviary.utils.aviary_values import AviaryValues
 from aviary.variable_info.functions import setup_model_options
-from aviary.variable_info.options import get_option_defaults
 from aviary.variable_info.variables import Aircraft
 
 tol = 5e-4
@@ -194,7 +194,7 @@ class TestEmpennageGroup(
         assert_check_partials(partial_data, **partial_tols)
 
     def test_large_sinle_aisle_1_calc_volcoefs(self):
-        options = get_option_defaults()
+        options = AviaryValues()
         options.set_val(Aircraft.Design.COMPUTE_HTAIL_VOLUME_COEFF, val=True, units='unitless')
         options.set_val(Aircraft.Design.COMPUTE_VTAIL_VOLUME_COEFF, val=True, units='unitless')
 
